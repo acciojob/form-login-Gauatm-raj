be566d3d775defb4da38bbe725971b5b3434295f
Gauatm-raj <153638242+Gauatm-raj@users.noreply.github.com>
2024-08-14 20:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    event.preventDefault(); // Prevent the form from submitting in the traditional way
 
+    // Get the form elements by their names
+    let form = document.getElementById('form1');
+    let fname = form.elements['fname'].value;
+    let lname = form.elements['lname'].value;
+
+    // Display the alert with the concatenated first and last name
+    alert(`${fname} ${lname}`);
 }
+
+// Attach the event listener to the form's submit event
+document.getElementById('form1').addEventListener('submit', getFormvalue);
